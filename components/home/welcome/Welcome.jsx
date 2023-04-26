d40e556e742b6a44204a1dbc93b20b4ab8d2238d
smschijf <210578@student.glu.nl>
2023-04-26 12:03:59 +0200

    search input added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    FlatList
+  } from 'react-native';
+  import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Sander</Text>
+        <Text style={styles.welcomeMessage}>Find your new game</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} value="" onChange={() => {}} placeholder="What are you looking for?" />
+        </View>
+        <TouchableOpacity>
+          <Image source={icons.search} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
